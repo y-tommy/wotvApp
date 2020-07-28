211d82ddefb1a61f7304bf000de0d6d323aeebcf
y-tommy <yuji.tomie@gmail.com>
2020-07-29 00:40:55 +0900

    create calc script

--- app/javascript/jquery/jquery.js
+++
@@ -1,8 +0,0 @@
-function damage(){
-  var atk = $("field1").val();
-  var pow = $("field2").val();
-  var res = $("field3").val();
-  var def = $("field4").val();
-  var total = atk*pow;
-  $('field_total').val('total');
-};
